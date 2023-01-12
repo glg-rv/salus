a8d0a5ce7d69ab4f5a53e9396db37987bd889d5a
Gianluca Guida <gianluca@rivosinc.com>
2023-01-12 10:36:01 +0000

    salus: call u-mode on RivosTest::MemCopy
    Signed-off-by: Gianluca Guida <gianluca@rivosinc.com>

--- src/vm.rs
+++ src/vm.rs
@@ -16,6 +16,8 @@ use s_mode_utils::print::*;
 use sbi_rs::{Error as SbiError, *};
 
 use crate::guest_tracking::{GuestStateGuard, GuestVm, Guests};
+use crate::hyp_map::UmodeMapping;
+use crate::umode::UmodeTask;
 use crate::vm_cpu::{ActiveVmCpu, VmCpu, VmCpuParent, VmCpuStatus, VmCpuTrap, VmCpus, VM_CPUS_MAX};
 use crate::vm_pages::Error as VmPagesError;
 use crate::vm_pages::{
@@ -725,8 +727,7 @@ impl<'a, T: GuestStagePagingMode> FinalizedVm<'a, T> {
             }
             SbiMessage::Pmu(pmu_func) => self.handle_pmu_msg(pmu_func, active_vcpu).into(),
             SbiMessage::RivosTest(test_function) => {
-                // TODO
-                EcallAction::Unhandled
+                self.handle_test(test_function, active_vcpu.active_pages())
             }
         }
     }
@@ -1480,6 +1481,56 @@ impl<'a, T: GuestStagePagingMode> FinalizedVm<'a, T> {
             .vm_pages()
             .initiate_fence()
             .map_err(EcallError::from)?;
+        Ok(0)
+    }
+
+    fn handle_test(
+        &self,
+        test_func: RivosTestFunction,
+        _active_pages: &ActiveVmPages<T>,
+    ) -> EcallAction {
+        use RivosTestFunction::*;
+        match test_func {
+            MemCopy(args) => self.guest_test_memcopy(args.to, args.from, args.len).into(),
+        }
+    }
+
+    fn map_guest_range_in_umode_slot(
+        &self,
+        slot: u64,
+        addr: u64,
+        len: u64,
+        writable: bool,
+    ) -> EcallResult<(u64, UmodeMapping)> {
+        let base = PageSize::Size4k.round_down(addr);
+        let end = addr
+            .checked_add(len)
+            .ok_or(EcallError::Sbi(SbiError::InvalidParam))?;
+        let pages = self
+            .vm_pages()
+            .pin_shared_pages(
+                self.guest_addr_from_raw(base)?,
+                PageSize::num_4k_pages(end - base),
+            )
+            .map_err(EcallError::from)?;
+        let mapping = UmodeMapping::new(slot, pages, writable)
+            .map_err(|_| EcallError::Sbi(SbiError::Failed))?;
+        let vaddr = mapping.vaddr().bits() + (addr - base);
+        Ok((vaddr, mapping))
+    }
+
+    fn guest_test_memcopy(&self, to: u64, from: u64, len: u64) -> EcallResult<u64> {
+        let overlap = core::cmp::max(to, from) <= core::cmp::min(to + len - 1, from + len - 1);
+        if overlap {
+            return Err(EcallError::Sbi(SbiError::InvalidParam));
+        }
+        let (from_vaddr, _from_mapping) =
+            self.map_guest_range_in_umode_slot(0, from, len, false)?;
+        let (to_vaddr, _to_mapping) = self.map_guest_range_in_umode_slot(1, to, len, true)?;
+        // Safe because we checked that the two ranges do not overlap, and we mapped them for at
+        // least `len` bytes.
+        let request = unsafe { u_mode_api::UmodeRequest::memcopy(to_vaddr, from_vaddr, len) };
+        UmodeTask::send_req(request).map_err(|_| EcallError::Sbi(SbiError::Failed))?;
         Ok(0)
     }
 
